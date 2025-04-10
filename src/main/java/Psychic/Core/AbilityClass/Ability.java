bbe5694c2f22aa8dcbfeb4df3875fd5567ee903e
mozzamaru <kanghyun486111@gmail.com>
2025-04-10 22:15:52 +0900

    :sparkles: 모두에게 능력 표시

--- src/main/java/Psychic/Core/AbilityClass/Ability.java
+++ src/main/java/Psychic/Core/AbilityClass/Ability.java
@@ -11,12 +11,20 @@ public abstract class Ability implements AbilityConcept, Listener {
     @Override
     public void apply(Player player) {
         Bukkit.getPluginManager().registerEvents(this, Psychic.getInstance());
-        player.sendMessage("§c[능력] " + getClass().getSimpleName() + " 능력이 부여되었습니다!");
+        String abilityName = getClass().getSimpleName();
+        String playerName = player.getName();
+
+        // 전체에게 브로드캐스트
+        Bukkit.broadcastMessage("§c[능력] " + playerName + "님에게 " + abilityName + " 능력이 부여되었습니다!");
     }
 
     @Override
     public void remove(Player player) {
         HandlerList.unregisterAll(this);
-        player.sendMessage("§7[능력] " + getClass().getSimpleName() + " 능력이 제거되었습니다.");
+        String abilityName = getClass().getSimpleName();
+        String playerName = player.getName();
+
+        // 전체에게 브로드캐스트
+        Bukkit.broadcastMessage("§7[능력] " + playerName + "님의 " + abilityName + " 능력이 제거되었습니다.");
     }
 }
