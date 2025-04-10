d22030948fb9b56712abf6a4b03ea238155733bd
mozzamaru <kanghyun486111@gmail.com>
2025-04-10 22:58:16 +0900

    :rocket: 설명 추가

--- src/main/java/Psychic/Ability/kotlin.kt
+++ src/main/java/Psychic/Ability/kotlin.kt
@@ -23,11 +23,12 @@ class kotlin : Ability() {
     class Info : AbilityInfo() {
         //꼭 setupItems여야함
         override fun setupItems() {
-            addItem(0, Material.ENCHANTED_BOOK, "&b&lKotlin", "&7이곳은 샘플 능력입니다.", "&7능력 개발을 위한 참고용입니다.")
+            addItem(0, Material.ENCHANTED_BOOK, "&b&lKotlin", "&7이것은 샘플 능력입니다.", "&7능력 개발을 위한 참고용입니다.")
             addItem(
                 2, Material.STICK, "&2&l샘플 클래스 ACTIVE",
-                "이곳은 샘플 능력입니다.", "히트스캔 방식으로 구현해뒀습니다",
-                "혹시나 능력을 쓸일이 있으시다면 이 코드 참조하시면 됩니다."
+                "히트스캔 방식이 필요하신분들은 이 코드를 참조해 주세요.",
+                "코틀린 형식으로 작성된 샘플이기 때문에 자바형식이 필요하시다면",
+                "thanos클래스나 magicarchery클래스를 참조하세요"
             )
         }
     }
