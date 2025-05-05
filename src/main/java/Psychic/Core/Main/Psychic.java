fd4c9f6c2c813fe50430c463721bdce9896b8d15
nomusamo <kanghyun486111@gmail.com>
2025-05-06 02:39:30 +0900

    :fire: Commit!

--- src/main/java/Psychic/Core/Main/Psychic.java
+++ src/main/java/Psychic/Core/Main/Psychic.java
@@ -3,21 +3,15 @@ package Psychic.Core.Main;
 import Psychic.Core.AbilityDamage.LevelForArmor;
 import Psychic.Core.AbilityDamage.LevelForDamage;
 import Psychic.Core.AbilityEffect.AbilityFireWorkDamage;
+import Psychic.Core.AbilityEffect.AbilitySnowballKnockBack;
 import Psychic.Core.Command.Pommand;
 import Psychic.Core.Command.PsyTabCompleter;
 import Psychic.Core.Manager.Mana.Join;
 import Psychic.Core.Manager.Mana.ManaManager;
-import com.destroystokyo.paper.event.entity.EntityKnockbackByEntityEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Snowball;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
 
-public final class Psychic extends JavaPlugin implements Listener {
+public final class Psychic extends JavaPlugin{
     private static Psychic instance;
 
     @Override
@@ -38,7 +32,7 @@ public final class Psychic extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(new LevelForDamage(), this);
         Bukkit.getPluginManager().registerEvents(new AbilityFireWorkDamage(), this);
         Bukkit.getPluginManager().registerEvents(new Gui(), this);
-        Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(new AbilitySnowballKnockBack(), this);
         Bukkit.getPluginManager().registerEvents(new Join(), this);
     }
 
@@ -51,17 +45,5 @@ public final class Psychic extends JavaPlugin implements Listener {
     public static Psychic getInstance() {
         return instance;
     }
-    @EventHandler
-    public void onKnockback(EntityKnockbackByEntityEvent event) {
-        Entity entity = event.getEntity();
-        if (!(entity instanceof LivingEntity)) return;
 
-        Entity damager = event.getHitBy();
-        if (!(damager instanceof Snowball)) return;
-
-        if (!damager.hasMetadata("noKnockback")) return;
-
-        // 넉백 제거
-        event.setKnockback(new Vector(0, 0, 0));
-    }
 }
