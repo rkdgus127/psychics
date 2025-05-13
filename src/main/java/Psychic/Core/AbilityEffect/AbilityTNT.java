c21809be9a41e8fff42f10bc1b31b5253bd32bd1
nomusamo <kanghyun486111@gmail.com>
2025-05-13 23:31:53 +0900

    :recycle: 폭발 메소드 추가!

---
+++ src/main/java/Psychic/Core/AbilityEffect/AbilityTNT.java
@@ -0,0 +1,29 @@
+package Psychic.Core.AbilityEffect;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+public class AbilityTNT implements Listener {
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof TNTPrimed) {
+            if (event.getDamager().hasMetadata("Bomber")) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onTNTExplode(EntityExplodeEvent event) {
+        if (event.getEntityType() == EntityType.TNT) {
+            if (event.getEntity().hasMetadata("Bomber")) {
+                event.setCancelled(true);
+            }
+        }
+    }
+}
