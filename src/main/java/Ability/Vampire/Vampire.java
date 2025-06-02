f683cdcd5b391443907080d1ef61c66b07c3eb59
nomusamo <kanghyun486111@gmail.com>
2025-06-02 21:34:46 +0900

    commit test

--- src/main/java/Ability/Vampire/Vampire.java
+++ src/main/java/Ability/Vampire/Vampire.java
@@ -52,16 +52,16 @@ public class Vampire extends Ability {
         double damage = event.getFinalDamage();
         double heal = damage * Heal_Multy/100;
 
-        // 공격시 주위에 레드스톤 드랍
+
         ItemStack bloodDrop = new ItemStack(Material.REDSTONE);
         ItemMeta meta = bloodDrop.getItemMeta();
         meta.setDisplayName("§4§l핏 방울");
 
-        // 레드스톤에 데미지 입력
+
         meta.getPersistentDataContainer().set(DAMAGE_KEY, PersistentDataType.DOUBLE, heal);
         bloodDrop.setItemMeta(meta);
 
-        // 레드스톤 드롭 및 이름
+
         Item dropped = target.getWorld().dropItem(target.getLocation(), bloodDrop);
         dropped.setCustomName("§4§l핏 방울");
         dropped.setCustomNameVisible(true);
@@ -78,18 +78,17 @@ public class Vampire extends Ability {
         ItemMeta meta = stack.getItemMeta();
         if (!meta.getDisplayName().equals("§4§l핏 방울")) return;
 
-        // 뱀파이어만 줍기 가능
-        // * 단 다른 뱀파이어도 스틸 가능 *
+
         if (!AbilityManager.hasAbility(player, Vampire.class)) {
             event.setCancelled(true);
             return;
         }
 
-        // 저장된 데미지 출력
+
         Double heal = meta.getPersistentDataContainer().get(DAMAGE_KEY, PersistentDataType.DOUBLE);
         if (heal == null) return;
 
-        // 체력 회복
+
         double newHealth = Math.min(player.getHealth() + heal, player.getMaxHealth());
         player.setHealth(newHealth);
         World world = player.getWorld();
